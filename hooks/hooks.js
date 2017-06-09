61c336a94f0709934fdba94a2d7ccd6d28f59015
wonknu10 <fabrice.labbe@adfab.fr>
2017-06-09 15:07:50 +0200

    change icon text icon

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -4,7 +4,7 @@ var hooks = {
   afterEditorInput: function (htmlString, params, abe) {
     if(params.datepicker != null) {
       htmlString = htmlString.replace('form-control form-abe', 'form-control form-abe datepicker')
-                             .replace('glyphicon-font', 'glyphicon-eye-open')
+                             .replace('glyphicon-font', 'glyphicon-calendar')
                              .replace('class="form-control form-abe', 'data-format="' + ((params.format != null) ? params.format : 'DD-MM-YYYY') + '" class="form-control form-abe');
     }
 
